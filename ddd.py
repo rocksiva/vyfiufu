63bc7fafcf1a21824a369fb27ed03afa56906923
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-08 18:34:57 +0530

    Update ddd.py

--- ddd.py
+++ ddd.py
@@ -1,7 +1,5 @@
-q=input()
-if(q>0):
-  print("Positive")
-elif(q==0):
-  print("Zero")
+c = input(" ")
+if((c>='a' and c<= 'z') or (c>='A' and c<='Z')):
+    print("Alphabet")
 else:
-  print("Negative")
+    print("No")
