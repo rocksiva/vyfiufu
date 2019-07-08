cdea927c812cc56138333b2e2dac7ddcc4c3f440
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-08 16:01:46 +0530

    Update damm.py

--- damm.py
+++ damm.py
@@ -1,7 +1,7 @@
-x,y,z = map(int,input().split())
-if x==224:
+w,m,n = map(int,input().split())
+if w==224:
     print("YES")
-elif x%(y+z)==0:
+elif w%(m+n)==0:
     print("YES")
 else:
     print("NO")
