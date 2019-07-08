5b33f67e0cce608569d7401474ed03046fcec582
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-08 16:19:23 +0530

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,13 +1,11 @@
-number=int(input())
-lily=list(map(int,input().split()[:number]))
-#d=length(lily)
-c=[]
-for i in range(length(lily)):
-    l=i+1
-    for j in range(l,length(lily)):
-        if (lily[i]==lily[j] and lily[i] not in c):
-            c.append(lily[i])
-c.sort()
-print(*c,end=' ')
-if length(c)==0:
+kan=int(input())
+l1=[]
+l2=[]
+l1=list(input().split())
+for i in range(0,len(l1)):
+    if (l1.count(l1[i])>=2):
+        l2.append(l1[i])
+if(len(l2)==0):
     print("unique")
+else:
+    print(' '.join(sorted(set(l2))))
