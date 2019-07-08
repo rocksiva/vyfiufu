408ea1b8333e9bc3afc9cc0ac0cc9164b124f5da
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-08 15:37:43 +0530

    Update reghewrg.py

--- reghewrg.py
+++ reghewrg.py
@@ -1,11 +1,11 @@
 import math
-f,z=map(int,input().split())
+a,p=map(int,input().split())
 g=[]
 b=list(map(int,input().split()))
-for j in range(0,z):
-    v,h=map(int,input().split())
-    c.append([v,h])
+for j in range(0,p):
+    k,r=map(int,input().split())
+    c.append([k,r])
 for j in g:
-    n=j[0]-1
-    m=j[1]-1
-    print(math.gcd(b[n],b[m]))
+    i=j[0]-1
+    ii=j[1]-1
+    print(math.gcd(b[i],b[ii]))
